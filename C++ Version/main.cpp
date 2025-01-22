54447fbcf0a12d32f999f039652b979b191b57ee
Vadym <107150489+Vadim0036@users.noreply.github.com>
2025-01-21 21:45:01 -0600

    snake game main function

---
+++ C++ Version/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Snake_Game.cpp"
+
+int main(void)
+{
+    SnakeGame game;
+
+    game.start_game();
+
+    //std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
