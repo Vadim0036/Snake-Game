f582348b8819ef2ed24483f9bb894b3741ce909d
Vadym <107150489+Vadim0036@users.noreply.github.com>
2025-01-21 21:49:33 -0600

    Update main.c

--- C Version/main.c
+++ C Version/main.c
@@ -1,1 +1,12 @@
+/* Test */
 
+/* This Version of Snake Game has not been started */
+
+/* I am currently working on C++ Version */
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello World\n");
+}
